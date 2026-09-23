6adad3ac8c53d3294c589f9b8b4c3582438a7aae
Hananana43 <lhannah.lubin@gmail.com>
2026-09-23 14:51:19 +0000

    oiho

--- week5/LLN.java
+++ week5/LLN.java
@@ -2,7 +2,7 @@ package week5linkedlist;
 
 public class LLN<T>{
     private T value;
-    private LLN value;
+    private LLN next;
 
     public LLN(T value, LLN next){
         this.next=next;
@@ -12,7 +12,45 @@ public class LLN<T>{
     public T getValue(){
         return this.value;
     }
+    /* public String toString(){//while loop method
+        LLN<T> current = this;
+        String out = "";
 
+        while (current != null){
+            out += current.value+" ";
+            current = current.next;
+        }
+        return out;
+    }
+    */
+    public String toString(){ //recursive method
+        if (this.next == null){
+            return ""+this.value;
+        }else{
+            return this.value+" "+this.next.toString();
+            }
+
+    }
+
+    public void insert(T value, int loc){
+        if(loc==0){
+            this.next=new LLN(this.value, this.next);
+            this.value=value;
+        }else{
+            this.next.insert(value, loc-1);
+        }
+    }
+
+    public boolean find(T toFind){
+        if(this.value.equals(toFind)){
+            return true;
+        
+        }else if(this.next==null){
+            return false;
+        }else{
+            return this.next.find(toFind);
+        }
+    }
 
     public static void main (String[] args){
         LLN<Integer> head = new LLN<>(1, null);
@@ -20,4 +58,7 @@ public class LLN<T>{
         head=new LLN(3,head);
         System.out.println(head);
     }
+
+    //add a recusion that calls 3 2 1 
+
 }
